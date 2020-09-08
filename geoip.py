7d2145f6a4f67d5cf8bf2a273a9bd3e35ad86502
kernelpanic <66163332+kernelpanic32@users.noreply.github.com>
2020-09-08 17:01:21 -0300

    Add files via upload

--- geoip.py
+++ geoip.py
@@ -3,53 +3,84 @@ import sys
 import json
 import socket
 
-if (len(sys.argv) > 1):
-	
-	if(sys.argv[1] == "?"):
-		print('')
-		print('IP address information provider. Coded by SANGUB - 2020')
-		print('syntax: Leave blank to provide your own ip info or enter another ip ex.: "geoip 152.312.53.256"')
-
-	else:
-
-		try:
-			socket.inet_aton(sys.argv[1])
-			try:
-				data = urlopen('http://ipinfo.io/'+sys.argv[1]).read()
-				data = json.loads(data)
-
-				print('')		
-				print('IP: 			'+data['ip'])
-				print('City: 			'+data['city'])
-				print('Region: 		'+data['region']+' ['+data['country']+']')
-				print('Avg. Location: 		'+data['loc'])
-				print('ISP: 			'+data['org'])
-
-			except:
-				print('')
-				print('An error has ocurred, try again later.')
-		except:
-			print('')
-			print('Please enter a valid IP address.')
-
-else:
-
+def GetPublicIP():
+	#gets your own public ip address
 	try:
-		remote_ip = urlopen('http://api.ipify.org').read()
+		public_ip = urlopen('http://api.ipify.org').read()
+		return public_ip
+	except:
+		print('An error has ocurred while retrieving public ip address, try again later.')
+
+def GetData(ip_address):
+	try:
+		socket.inet_aton(ip_address)
 		try:
-			data = urlopen('http://ipinfo.io/'+remote_ip).read()
+			data = urlopen('http://ipinfo.io/'+ip_address).read()
 			data = json.loads(data)
-
-			print('')		
-			print('IP: 			'+data['ip'])
-			print('City: 			'+data['city'])
-			print('Region: 		'+data['region']+' ['+data['country']+']')
-			print('Avg. Location: 		'+data['loc'])
-			print('ISP: 			'+data['org'])
-
+			return data
 		except:
 			print('')
 			print('An error has ocurred, try again later.')
 	except:
 		print('')
 		print('Please enter a valid IP address.')
+
+def PrintData(data):
+
+	countryCodes = []
+	countryNames = []
+	continentNames = []
+
+	#if country codes json exists then outputs more specific region location
+	try:
+		with open('country_codes.json') as country_codes:
+			json_data = json.load(country_codes)
+			all_countries = json_data["country"]
+
+			for json_row in all_countries:
+
+				if data['country'] == json_row["countryCode"]:
+					countryCodes.append(json_row["countryCode"])
+					countryNames.append(json_row["countryName"])
+					continentNames.append(json_row["continentName"])
+					break
+
+		region = data['region']+', '+json_row['countryName']+' ['+json_row["countryCode"]+'], '+json_row['continentName']
+
+	except IOError:
+		#if country codes json does not exist then outputs simple code
+		region = data['region']+' ['+data['country']+']'
+		print('Country codes file not accessible')
+
+	finally:
+		country_codes.close()
+
+
+	try:
+		print('')		
+		print('IP: 			'+data['ip'])
+		print('City: 			'+data['city'])
+		print('Region: 		'+region)
+		print('Avg. Location: 		'+data['loc'])
+		print('ISP: 			'+data['org'])
+
+	except:
+		print('')
+		print('An error has ocurred, try again later.')
+
+if (len(sys.argv) > 1):
+	
+	if(sys.argv[1] == "?"):
+		print('')
+		print('syntax: "geoip" or "geoip xxx.xxx.xxx.xxx"')
+	else:
+
+		#remote public ip info
+		data = GetData(sys.argv[1])
+		PrintData(data)
+
+else:
+	#own public ip info
+	data = GetData(GetPublicIP())
+	PrintData(data)
+	
